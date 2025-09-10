7a4978c34da07e17c0e73494b3a7f98ba2aecda0
YGOR DOS PASSOS LUCIANO <ygor_luciano@estudante.sesisenai.org.br>
2025-09-10 19:49:52 -0300

    ygor

--- Multi-Page/src/layout/Header/Header.jsx
+++ Multi-Page/src/layout/Header/Header.jsx
@@ -3,20 +3,22 @@ import { NavLink } from "react-router";
 
 const Header = () => {
     return (
-        <div>
+        <header className='flex items-center justify-between p-4 bg-gray-200'>
             <nav className='flex gap-4'>
-                <NavLink to="/" end>
+                <NavLink to="/" end className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-gray-800"}>
                     Home
                 </NavLink>
-                <NavLink to="/sobre" end>
+
+                <NavLink to="/sobre" end className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-gray-800"}>
                     Sobre
                 </NavLink>
-                <NavLink to="/blog" end>
-                   Blog
+
+                <NavLink to="/blog" end className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-gray-800"}>
+                    Blog
                 </NavLink>
 
             </nav>
-        </div>
+        </header>
     )
 }
 
--- Multi-Page/src/pages/Blog/Blog.jsx
+++ Multi-Page/src/pages/Blog/Blog.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 const Blog = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/posts')
+    .then(res => res.json())
+    .then(data =>{
+      setPosts(data)
+    })
+  }, [])
+
   return (
     <>
-
-      <h1>Página Blog</h1>
+      <div className='flex gap-2'>
+        {
+          posts && posts.map(post =>(
+            <div key={post.id} className='card'>
+              <h2>{post.title}</h2>
+              <p>Views: {post.views}</p> 
+            </div>
+          ))
+        }
+      </div>
+      
     </>
   )
 }
